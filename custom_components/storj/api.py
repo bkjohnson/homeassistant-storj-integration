eead421257fef15395fcc941b59e100a6bdb76dd
Brooks Johnson <brooks.johnson@pm.me>
2025-02-20 18:10:46 -0800

    Revert "Add commit to test caching"
    This reverts commit 10c2d74dd994553d14d4390bb145c8c1df7ecdf4.

--- custom_components/storj/api.py
+++ custom_components/storj/api.py
@@ -34,7 +34,6 @@ class StorjClient:
         )
         await result.communicate()
 
-        # Testing cache
         return result.returncode == 0
 
     async def satelitte_is_live(self) -> bool:
